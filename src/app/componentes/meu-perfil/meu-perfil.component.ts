2c920f19336a545bbcc49457fff8e3e1297a393b
Mauricio Oran <mauricioperes46@gmail.com>
2023-06-19 15:51:49 -0400

    wip:profile page is working

--- src/app/componentes/meu-perfil/meu-perfil.component.ts
+++ src/app/componentes/meu-perfil/meu-perfil.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-meu-perfil',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./meu-perfil.component.css']
 })
 export class MeuPerfilComponent {
+  userForm: FormGroup;
+  selectedOption: string;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.userForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      surname: ['', Validators.required],
+      mat: ['', Validators.required],
+      date: [null],
+      street: ['', Validators.required],
+      neigh: ['', Validators.required],
+      cep: ['', Validators.required],
+      number: ['', Validators.required],
+      course: ['', Validators.required],
+      description: ['', Validators.required],
+      phone: ['', Validators.required],
+      gender: ['', Validators.required]
+    });
+
+    this.selectedOption = '';
+  }
+
+  submitForm() {
+    if (this.userForm.invalid) {
+      return;
+    }
+
+    // Lógica para enviar o formulário
+  }
+
+  selectOption(option: string) {
+    this.selectedOption = option;
+  }
 }
--- src/app/componentes/meu-perfil/meu-perfil.module.ts
+++ src/app/componentes/meu-perfil/meu-perfil.module.ts
@@ -3,8 +3,13 @@ import { CommonModule } from '@angular/common';
 import { MeuPerfilComponent } from './meu-perfil.component';
 import { MeuPerfilRoutingModule } from './meu-perfil-routing.module';
 import { NavBarModule } from '../nav-bar/nav-bar.module';
-
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -13,7 +18,16 @@ import { NavBarModule } from '../nav-bar/nav-bar.module';
   imports: [
     CommonModule,
     MeuPerfilRoutingModule,
-    NavBarModule
+    MatInputModule,
+    NavBarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatOptionModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ]
 })
 export class MeuPerfilModule { }
